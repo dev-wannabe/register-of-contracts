396d72541e6792e667060b1fadf3ce5bf8c5ecc1
Marek <looppega@gmail.com>
2018-05-11 12:55:16 +0200

    Initial commit

---
+++ src/main/java/pl/devwannabe/registerOfContracts/RegisterOfContractsApplication.java
@@ -0,0 +1,12 @@
+package pl.devwannabe.registerOfContracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterOfContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterOfContractsApplication.class, args);
+	}
+}
