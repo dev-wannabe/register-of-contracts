f0aa3c79e88a325fba58fa079e57c629f9259326
Marek <looppega@gmail.com>
2018-05-15 17:21:26 +0200

    security

--- src/main/java/pl/devwannabe/RegisterOfContractsApplication.java
+++ src/main/java/pl/devwannabe/RegisterOfContractsApplication.java
@@ -1,12 +1,16 @@
-package pl.devwannabe.registerofcontracts;
+package pl.devwannabe;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import pl.devwannabe.registerofcontracts.configuration.SecurityConfig;
+import pl.devwannabe.registerofcontracts.controller.MainController;
+
 @SpringBootApplication
 //@ComponentScan(basePackageClasses = MainController.class)
-@ComponentScan
+//@ComponentScan
 public class RegisterOfContractsApplication {
 
 	public static void main(String[] args) {
