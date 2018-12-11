5aa1f54606f5b88154ba04a063ace0b9e3b1a7f6
Marek <looppega@gmail.com>
2018-12-11 18:18:32 +0100

    Registration process in progress

--- src/main/java/pl/devwannabe/services/security/UserServiceImpl.java
+++ src/main/java/pl/devwannabe/services/security/UserServiceImpl.java
@@ -1,16 +1,35 @@
 package pl.devwannabe.services.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 import pl.devwannabe.domain.Model.User;
+import pl.devwannabe.domain.repository.RoleRepository;
+import pl.devwannabe.domain.repository.UserRepository;
 
-public class UserServiceImpl implements UserService{
+import java.util.HashSet;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public void save(User user) {
-
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user);
     }
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username);
     }
 }
